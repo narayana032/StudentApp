afc1736e31df444c731e36ac86864b330f3474f3
Narayana Jampani <narayana032@gmail.com>
2018-02-03 21:30:42 +0530

    Create implemented class for StudentApp

---
+++ src/com/sapp/StudentAppImpl.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.sapp;
+
+import com.sapp.model.Student;
+
+/**
+ * @author Admin
+ *
+ */
+public class StudentAppImpl implements StudentApp {
+
+	/* (non-Javadoc)
+	 * @see com.sapp.StudentApp#addStudent(com.sapp.model.Student)
+	 */
+	@Override
+	public void addStudent(Student aStudent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.sapp.StudentApp#deleteStudent(java.lang.String)
+	 */
+	@Override
+	public boolean deleteStudent(String studentId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.sapp.StudentApp#updateStudent(com.sapp.model.Student)
+	 */
+	@Override
+	public Student updateStudent(Student uStudent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.sapp.StudentApp#getStudentInfo(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public Student getStudentInfo(String id, String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
